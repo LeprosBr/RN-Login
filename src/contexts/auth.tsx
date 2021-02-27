714294a3391b5908a1eb3bb6dc488eea4a25fafb
Kaio Gama <kaioeduardoescar@gmail.com>
2021-02-27 17:53:06 -0400

    [Routes] -- SignIn use COntext test ok
    Signed-off-by: Kaio Gama <kaioeduardoescar@gmail.com>

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { createContext } from "react";
 import * as auth from "../services/auth";
 
 interface AuthContextData {
   signed: boolean;
-  user: object;
+  user: object | null;
   signIn(): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<object | null>(null);
+
   async function signIn() {
     const response = await auth.signIn();
-
-    console.log(response);
+    setUser(response.user);
   }
 
   return (
-    <AuthContext.Provider value={{ signed: false, user: {}, signIn }}>
+    <AuthContext.Provider value={{ signed: !!user, user: user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
