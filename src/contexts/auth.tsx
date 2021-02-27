6da0bbaf20fb23ab6b09e37ae2c3cb3ac51bf1a4
Kaio Gama <kaioeduardoescar@gmail.com>
2021-02-27 19:10:52 -0400

    [Routes] -- AsyncStorage
    Signed-off-by: Kaio Gama <kaioeduardoescar@gmail.com>

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createContext } from "react";
 import * as auth from "../services/auth";
+import AsyncStorage from "@react-native-community/async-storage";
 
 interface AuthContextData {
   signed: boolean;
@@ -14,13 +15,27 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 export const AuthProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState<object | null>(null);
 
+  useEffect(() => {
+    async function loadStorageData() {
+      const storageUser = await AsyncStorage.getItem("@RNAuth:user");
+      const storageToken = await AsyncStorage.getItem("@RNAuth:token");
+
+      if (storageUser && storageToken) {
+        setUser(JSON.parse(storageUser));
+      }
+    }
+    loadStorageData();
+  }, []);
+
   async function signIn() {
     const response = await auth.signIn();
     setUser(response.user);
   }
 
   async function signOut() {
-    setUser(null);
+    AsyncStorage.clear().then(() => {
+      setUser(null);
+    });
   }
 
   return (
